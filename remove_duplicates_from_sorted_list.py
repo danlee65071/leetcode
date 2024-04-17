102fa5e7cd3adaa4d1bbf4230213924a3ed03ced
DanLee <danlee65071@gmail.com>
2024-04-17 19:13:29 +0300

    remove_duplicates_from_sorted_list

---
+++ remove_duplicates_from_sorted_list.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val: int = val
+        self.next: ListNode = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy: ListNode = ListNode()
+        dummy.next = head
+        cur: ListNode = dummy
+        while cur:
+            if cur.next and cur.next.next and cur.next.val == cur.next.next.val:
+                tmp: ListNode = cur.next.next
+                while tmp.next and tmp.val == tmp.next.val:
+                    tmp = tmp.next
+                cur.next.next = tmp.next
+            else:
+                cur = cur.next
+        return dummy.next
